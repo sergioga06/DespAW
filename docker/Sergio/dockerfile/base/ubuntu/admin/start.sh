ac57d5f4145adbca40bf9292e29660872a508f5d
sergioga06 <sergiogallardog06@gmail.com>
2025-11-10 14:18:14 +0100

    estructura

--- docker/Sergio/dockerfile/base/ubuntu/admin/start.sh
+++ docker/Sergio/dockerfile/base/ubuntu/admin/start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-tail -f /dev/null
+newUser(){
+    useradd -rm -d /home/sergio -s /bin/bash sergio
+    echo "sergio:1234" | chpasswd
+    echo "Bienvenido Sergio ..." > /home/sergio/bienvenida.txt
+}
+main(){
+    newUser
+    tail -f /dev/null
+}
+
+main
